5db8ffc21ab3e6c47ccbb5db6843de851d4b05a8
shtygarmaxim-glitch <shtygarmaxim@gmail.com>
2026-03-12 21:51:48 +0300

    Update server.js

--- rolls-game/server.js
+++ rolls-game/server.js
@@ -1,15 +1,43 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 const path = require('path');
-const app = express();
 
-const PORT = process.env.PORT || 10000;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.static(__dirname));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+let players = {};
+let currentBets = [];
+
+io.on('connection', (socket) => {
+    console.log('Игрок подключился:', socket.id);
+
+    // Когда игрок заходит, он отправляет свои данные из TG
+    socket.on('join', (userData) => {
+        players[socket.id] = { ...userData, socketId: socket.id };
+        io.emit('updatePlayers', Object.values(players));
+    });
+
+    // Создание ставки
+    socket.on('createBet', (amount) => {
+        const newBet = {
+            id: Math.random().toString(36).substr(2, 9),
+            creator: players[socket.id],
+            amount: amount,
+            status: 'waiting'
+        };
+        currentBets.push(newBet);
+        io.emit('newBetCreated', currentBets);
+    });
+
+    socket.on('disconnect', () => {
+        delete players[socket.id];
+        io.emit('updatePlayers', Object.values(players));
+    });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-});
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, () => console.log(`Server on port ${PORT}`));
